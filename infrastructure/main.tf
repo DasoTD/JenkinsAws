dac5d847c0b00dfbb35147a01ab82416ea5c50f1
xybuild <timileyindaso@gmail.com>
2024-08-28 11:45:59 +0100

    aws_subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -80,8 +80,8 @@ module "eks" {
 
 
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets 
+  vpc_id     = aws_vpc.example.id
+  subnet_ids = aws_subnet.example.id 
 
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"

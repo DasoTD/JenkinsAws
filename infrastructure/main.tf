ed7be366ccdfbda99c26a65f7acd6772d59f8643
xybuild <timileyindaso@gmail.com>
2024-08-28 07:51:52 +0100

    recalc

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,4 +1,68 @@
 module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "terra"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "terraeks"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access = true
+
+
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets 
+
+  eks_managed_node_group_defaults = {
+    ami_type       = "AL2_x86_64"
+  }
+
+  eks_managed_node_groups = {
+    node1 = {
+      instance_types = ["t2.xlarge"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+    }
+
+
+    node2 = {
+      instance_types = ["t2.xlarge"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+
+
+}
+
+/*module "vpc" {
     source               = "./modules/vpc"
     vpc_cidr             = var.vpc_cidr
     vpc_name             = var.vpc_name
@@ -11,7 +75,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "capstoneWithJenkins"
+  cluster_name    = "capstonewithjenkins"
   cluster_version = "1.30"
 
   cluster_endpoint_public_access = true
@@ -47,4 +111,4 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
 
 
-}
+}*/
